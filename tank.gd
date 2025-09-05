2874b0a26f33efdf510e203d143a69107c55460e
Tyler Williams <45677560+Williams-T@users.noreply.github.com>
2025-09-05 17:05:28 -0400

    projectile addition
    added projectiles with trails
    they basically work although depending on the tank angle it kind of gets misaligned a little bit

--- tank.gd
+++ tank.gd
@@ -20,6 +20,19 @@ var rotation_divisor = 300.0
 var aim_angle = -90.0
 var aim_mod = 0.0
 
+var power = 500.0
+
+var projectile_counter = 0
+var turret_origin_offset = Vector2(0, -10)
+var turret_length = Vector2(50,0)
+
+var cached_trajectory: Array[Vector2] = []
+var last_aim_angle: float = -999.0
+var last_position: Vector2 = Vector2(-999, -999)
+var last_power_level : float = -999.0
+var show_trajectory: bool = false
+var perma_show_trajectory : bool = false
+
 func _ready() -> void:
 	if body_sprite:
 		adjust_collision_bounds(body_sprite.texture)
@@ -42,8 +55,29 @@ func _physics_process(delta: float):
 	else:
 		if Input.is_action_pressed("aim_left"):
 			aim_mod = clamp(aim_mod - 1.0, -66.0, 66.0)
+			update_trajectory_cache()
 		elif Input.is_action_pressed("aim_right"):
 			aim_mod = clamp(aim_mod + 1.0, -66.0, 66.0)
+			update_trajectory_cache()
+	if Input.is_action_pressed("power_up"):
+		power = clamp(power + 500.0*delta, 100.0, 1000.0)
+		update_trajectory_cache()
+	elif Input.is_action_pressed("power_down"):
+		power = clamp(power - 500.0*delta, 100.0, 1000.0)
+		update_trajectory_cache()
+	# Show trajectory while aiming
+	show_trajectory = Input.is_action_pressed("aim_left") or Input.is_action_pressed("aim_right") or Input.is_action_pressed("power_up") or Input.is_action_pressed("power_down")
+	
+	# Update trajectory if position changed significantly
+	if global_position.distance_to(last_position) > 10.0:
+		update_trajectory_cache()
+	
+	if Input.is_action_just_pressed("fire"):
+		var turret_tip = global_position + turret_origin_offset + Vector2(50, 0).rotated(global_rotation + deg_to_rad(aim_angle + aim_mod))
+		var firing_angle = global_rotation + deg_to_rad(aim_angle + aim_mod)
+		var p = Projectile.new(turret_tip, firing_angle, power)
+		p.projectile_hit.connect(_on_projectile_hit)
+		get_parent().add_child(p)
 	
 	# Move along the tank's forward direction (accounting for slope)
 	if direction != 0:
@@ -81,6 +115,26 @@ func _physics_process(delta: float):
 	# Move and slide
 	move_and_slide()
 	queue_redraw()
+
+func update_trajectory_cache():
+	var current_aim = aim_angle + aim_mod
+	
+	# Only recalculate if aim or position changed significantly
+	if abs(current_aim - last_aim_angle) > 0.5 or global_position.distance_to(last_position) > 5.0 \
+	or abs(power - last_power_level) > 0.5:
+		# Calculate turret tip in local space (exactly like drawing)
+		var local_turret_tip = turret_origin_offset + turret_length.rotated(deg_to_rad(current_aim))
+		# Transform to world space
+		var world_turret_tip = global_position + local_turret_tip.rotated(global_rotation)
+		# Firing angle is tank rotation + aim
+		var firing_angle = global_rotation + deg_to_rad(current_aim)
+		
+		var terrain = get_parent().get_node_or_null("Terrain")
+		cached_trajectory = Projectile.get_trajectory_arc(world_turret_tip, firing_angle, power, terrain, 15)
+		
+		last_aim_angle = current_aim
+		last_position = global_position
+		last_power_level = power
 
 func align_to_terrain(delta: float):
 	var left_hit = left_ray.is_colliding()
@@ -123,10 +177,26 @@ func align_to_terrain(delta: float):
 		if lowest_point < 5 and lowest_point > -5:
 			position.y += lowest_point
 
+func _on_projectile_hit(impact_pos: Vector2, impact_angle: float, target_type: String, target: Node):
+	print("Hit %s at %s with angle %s" % [target_type, impact_pos, rad_to_deg(impact_angle)])
+
 func get_aim_angle() -> float:
 	# For aiming your weapon later
 	return rotation
 
 func _draw() -> void:
-	draw_circle(Vector2(0, -10), 10.0, Color.AQUA)
-	draw_line(Vector2(0, -10), Vector2(50, 0).rotated(deg_to_rad(aim_angle + aim_mod)), Color.AQUAMARINE, 10.0)
+	if !body_sprite:
+		draw_rect(collision_shape.shape.get_rect(), Color.CADET_BLUE)
+	draw_circle(turret_origin_offset, 10.0, Color.AQUA)
+	draw_line(turret_origin_offset, turret_origin_offset + turret_length.rotated(deg_to_rad(aim_angle + aim_mod)), Color.AQUAMARINE, 10.0)
+	
+	# Draw cached trajectory (only while aiming)
+	# turned off for now, uncomment the following if statement and comment the other one to switch.
+	#if (show_trajectory and cached_trajectory.size() > 1): 
+	if perma_show_trajectory:
+		for i in range(cached_trajectory.size() - 1):
+			if i % 2 == 0:  # Every other point for performance
+				var local_start = to_local(cached_trajectory[i])
+				var local_end = to_local(cached_trajectory[i + 1])
+				draw_line(local_start, local_end, Color.WHITE, 2.0)
+	
--- terrain.gd
+++ terrain.gd
@@ -14,8 +14,13 @@ func _ready() -> void:
 	
 	var points : PackedVector2Array = []
 	points.append(Vector2(-3000, base_ground_level * 4))
-	for i in range(-3000, 3000, 10):
-		points.append(Vector2(i, base_ground_level + (slope_range * (noise1.get_noise_1d(i)+noise2.get_noise_1d(i)))))
+	for i in range(-3000, 3000, 10): # generate base terrain
+		points.append(Vector2(i, int(base_ground_level + (slope_range * (noise1.get_noise_1d(i)+noise2.get_noise_1d(i))))))
+	var neighbors = []
+	for ii in points.size(): # make terrain less jaggy
+		if ii != 0 and ii != points.size() - 1:
+			neighbors = [int(points[ii-1].y), int(points[ii+1].y)]
+			points[ii] = Vector2(points[ii].x, neighbors[0]).lerp(Vector2(points[ii].x, neighbors[1]), 0.5)
 	points.append(Vector2(3000, base_ground_level * 4))
 	poly.polygon = points
 	body_coll.polygon = points
